7e4eb8f92067264d605d156658268e2ea3921458
Hosain <contact@hosain.net>
2015-06-13 17:10:27 +0100

    Added VoatApplication for custom app setup

---
+++ app/src/main/java/net/hosain/voat/VoatApplication.java
@@ -0,0 +1,11 @@
+package net.hosain.voat;
+
+import android.app.Application;
+
+public class VoatApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
