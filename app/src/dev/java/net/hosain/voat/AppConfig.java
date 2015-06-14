5e2f0763419f1f2ba24b97ab2339bf754f9c1461
Hosain <contact@hosain.net>
2015-06-14 11:18:15 +0100

    Updated dev app host to test server

--- app/src/dev/java/net/hosain/voat/AppConfig.java
+++ app/src/dev/java/net/hosain/voat/AppConfig.java
@@ -1,6 +1,6 @@
 package net.hosain.voat;
 
 public class AppConfig {
-    public static final String VOAT_HOST = "http://fakevout.azurewebsites.net";
+    public static final String VOAT_HOST = "http://voat.hosain.net";
     public static final String VOAT_PUB_KEY = "test";
 }
