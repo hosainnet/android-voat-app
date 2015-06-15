3881334a7fced30cc5251ffa671f5401f9161657
Hosain <contact@hosain.net>
2015-06-15 14:33:24 +0100

    Replace Subverse ListView with a RecyclerView

--- app/src/main/java/net/hosain/voat/activity/subverse/SubverseAdapter.java
+++ app/src/main/java/net/hosain/voat/activity/subverse/SubverseAdapter.java
@@ -1,14 +1,52 @@
 package net.hosain.voat.activity.subverse;
 
 import android.content.Context;
-import android.widget.ArrayAdapter;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import net.hosain.voat.R;
+import net.hosain.voat.data.DataEntity;
 
 import java.util.List;
 
-public class SubverseAdapter<T> extends ArrayAdapter<Thread> {
+public class SubverseAdapter extends RecyclerView.Adapter<SubverseAdapter.DetailItemViewHolder> {
 
-    public SubverseAdapter(Context context, int resource, int textViewResourceId, List objects) {
-        super(context, resource, textViewResourceId, objects);
+    private List<DataEntity> mThreads;
+    private Context mContext;
+
+    public SubverseAdapter(Context context, List<DataEntity> threads) {
+        this.mThreads = threads;
+        this.mContext = context;
+    }
+
+    @Override
+    public DetailItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_subverse_list_item, parent, false);
+        return new DetailItemViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(DetailItemViewHolder holder, int position) {
+        holder.title.setText(mThreads.get(position).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mThreads.size();
+    }
+
+    public static class DetailItemViewHolder extends RecyclerView.ViewHolder {
+
+        protected TextView title;
+
+        public DetailItemViewHolder(View itemView) {
+            super(itemView);
+            this.title = (TextView) itemView.findViewById(R.id.subverse_item_title);
+        }
+
     }
 
 }
