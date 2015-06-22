110f7472ca0e5e7d4f2099f7186ee8d3d23d4db9
Hosain <contact@hosain.net>
2015-06-22 17:30:11 +0100

    Updated list activity to use AppCompactActivity

--- app/src/main/java/net/hosain/voat/activity/subverse/SubverseActivity.java
+++ app/src/main/java/net/hosain/voat/activity/subverse/SubverseActivity.java
@@ -1,18 +1,28 @@
 package net.hosain.voat.activity.subverse;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import net.hosain.voat.R;
 import net.hosain.voat.VoatApp;
 
-public class SubverseActivity extends ActionBarActivity {
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class SubverseActivity extends AppCompatActivity {
+
+    @InjectView(R.id.toolbar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thread_list);
         VoatApp.component.inject(this);
+        ButterKnife.inject(this);
+
+        setSupportActionBar(mToolbar);
 
     }
 
