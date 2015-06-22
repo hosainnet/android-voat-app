693a92872c0420809a0759e2a416f4b05b89ef6a
Hosain <contact@hosain.net>
2015-06-22 11:21:00 +0100

    Added action bar on list activity and some material styling

--- app/src/main/java/net/hosain/voat/activity/subverse/SubverseActivity.java
+++ app/src/main/java/net/hosain/voat/activity/subverse/SubverseActivity.java
@@ -1,12 +1,12 @@
 package net.hosain.voat.activity.subverse;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
 
 import net.hosain.voat.R;
 import net.hosain.voat.VoatApp;
 
-public class SubverseActivity extends FragmentActivity {
+public class SubverseActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
