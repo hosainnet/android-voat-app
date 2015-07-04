f2637588072ac6f04757ddf37c381d84aae76f1d
Hosain <contact@hosain.net>
2015-07-04 21:59:37 +0100

    Changed added navigation drawer with sample items

--- app/src/main/java/net/hosain/voat/activity/subverse/SubverseActivity.java
+++ app/src/main/java/net/hosain/voat/activity/subverse/SubverseActivity.java
@@ -1,8 +1,12 @@
 package net.hosain.voat.activity.subverse;
 
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import net.hosain.voat.R;
 import net.hosain.voat.VoatApp;
@@ -11,6 +15,9 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class SubverseActivity extends AppCompatActivity {
+
+    @InjectView(R.id.drawer)
+    DrawerLayout mDrawerLayout;
 
     @InjectView(R.id.toolbar)
     Toolbar mToolbar;
@@ -24,6 +31,19 @@ public class SubverseActivity extends AppCompatActivity {
 
         setSupportActionBar(mToolbar);
 
+        final ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
